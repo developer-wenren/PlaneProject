ddb83194d0b4b221830796fccd752edcb1a65053
Wrcj_12138 <wrcj12138@163.com>
2015-03-23 14:59:54 +0800

    游戏层

---
+++ Classes/GameScene/GameLayer/GameLayer.cpp
@@ -0,0 +1,9 @@
+//
+//  GameLayer.cpp
+//  Plane
+//
+//  Created by 软件开发 on 15/3/23.
+//
+//
+
+#include "GameLayer.h"
---
+++ Classes/GameScene/GameLayer/GameLayer.h
@@ -0,0 +1,14 @@
+//
+//  GameLayer.h
+//  Plane
+//
+//  Created by 软件开发 on 15/3/23.
+//
+//
+
+#ifndef __Plane__GameLayer__
+#define __Plane__GameLayer__
+
+#include <stdio.h>
+
+#endif /* defined(__Plane__GameLayer__) */
